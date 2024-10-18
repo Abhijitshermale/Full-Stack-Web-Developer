5278906f4b49a8d261370a9481126d171c231b44
AbhijitShermale <shrmaleabhijit99@gmail.com>
2024-10-18 10:35:25 +0530

    part 3 changes done

---
+++ Part 3/practice.js
@@ -0,0 +1,10 @@
+let n = 3
+let arr = [7, 9, 0, -2];
+console.log(arr.slice(0, n));
+console.log(arr.slice(arr.length - n, arr.length));
+console.log(arr.includes(10) ? 'available' : 'Not available');
+
+let str = "AbhIjit Shermale"
+
+console.log(str.length >= 0 ? 'String is not empty ' : 'String is empty');
+console.log(str.charAt(3).toLowerCase() === str.charAt(3) ? 'its in lower case':'Its in upper case');
